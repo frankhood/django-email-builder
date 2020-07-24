b2c94d46a198920872b779d961b4e5a207c4e17d
dvd.ria <d.ria@frankhood.it>
2020-07-24 11:25:29 +0200

    FB-37 aggiunta dist files

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-version = get_version("email_builder", "__init__.py")
+version = "0.1.0"
 
 
 if sys.argv[-1] == 'publish':
