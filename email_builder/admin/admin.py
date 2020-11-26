0271b78ab034cc8d7c2519d4d10e0002153b57ae
Davide Ria <d.ria@frankhood.it>
2020-11-26 11:19:11 +0100

    Change Submit Row in admin to remove save as new

--- email_builder/admin/admin.py
+++ email_builder/admin/admin.py
@@ -16,6 +16,11 @@ from email_builder.models import EmailBuilder, email_code_choices
 from email_builder.views import EmailBuilderHtmlPreview, EmailBuilderTxtPreview
 from email_builder.utils import get_email_builder_handler
 
+from django import template
+from django.template.context import Context
+
+register = template.Library()
+
 logger = logging.getLogger("email_builder")
 
 
@@ -141,3 +146,25 @@ class EmailBuilderAdmin(admin.ModelAdmin):
         return ""
 
     mail_preview.short_description = _("Preview")
+
+@register.inclusion_tag('admin/custom_submit_line.html', takes_context=True)
+def submit_row(context):
+    """
+    Displays the row of buttons for delete and save.
+    """
+    opts = context['opts']
+    change = context['change']
+    is_popup = context['is_popup']
+    save_as = context['save_as']
+    return {
+        'onclick_attrib': (opts.get_ordered_objects() and change
+                            and 'onclick="submitOrderForm();"' or ''),
+        'show_delete_link': (not is_popup and context['has_delete_permission']
+                              and (change or context['show_delete'])),
+        # 'show_save_as_new': False,
+        'show_save_and_add_another': context['has_add_permission'] and
+                            not is_popup and (not save_as or context['add']),
+        'show_save_and_continue': not is_popup and context['has_change_permission'],
+        'is_popup': is_popup,
+        'show_save': True
+    }
